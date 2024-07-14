6bec5cf92aaaa41697f9c97c1e3ff6ca767621d9
prottoy5 <106944019+prottoy5@users.noreply.github.com>
2024-07-14 14:54:19 +0600

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -27,10 +27,50 @@ echo "Welcome, " . $_SESSION['username'] . "!<br>";
         }
         .logout-button:hover {
             background-color: #cc0000;
+        }    
+    </style>
+    <style>
+        .user_info {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 16px;
+            color: white;
+            background-color: blue;
+            border: none;
+            border-radius: 5px;
+            text-align: center;
+            text-decoration: none;
+            cursor: pointer;
+            transition: background-color 0.3s;
         }
+        .user_info:hover {
+            background-color: darkblue;
+        }    
+    </style>
+    <style>
+        .edit_info {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 16px;
+            color: white;
+            background-color: blueviolet;
+            border: none;
+            border-radius: 5px;
+            text-align: center;
+            text-decoration: none;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+        .edit_info:hover {
+            background-color: blueviolet;
+        }    
     </style>
 </head>
 <body>
     <a href="logout.html" class="logout-button">Logout</a>
+    <a href="user_info.php" class="user_info">See User Info</a>
+    <a href="edit_info.html" class="edit_info">Edit Info</a>
 </body>
 </html>
+
+
